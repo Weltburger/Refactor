af2538b380b79ccb94d493768366dc6f88f630bd
Weltburger <pakowski398@yandex.ru>
2019-11-10 23:10:59 +0300

    firaa

--- 22.cs
+++ 22.cs
@@ -1,12 +1,4 @@
 if (Game1.clou == true)
  {Game1.clou = false;}
  else
-<<<<<<< HEAD
- { Game1.clou = true; } //aaa
-
- //aaa
-=======
- { Game1.clou = true; }
-
-//aaaa
->>>>>>> master
+ { Game1.clou = true; } 
